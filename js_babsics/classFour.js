30d760a5323470f471dd0803ccecfa7f647650e9
Naman Jindal <namanjindal113@gmail.com>
2022-10-13 21:09:51 +0530

    let, const, hoisting, scope

---
+++ js_babsics/classFour.js
@@ -0,0 +1,6 @@
+// console.log(true === 'true'); //
+// console.log(true == 'true'); //
+/* 
+    type casting ->
+*/
+
--- js_babsics/outputQuestions.js
+++ js_babsics/outputQuestions.js
@@ -108,4 +108,194 @@
 // }
 
 //scope chain and hjoisting with var 
+// var variable_2;//undefined
+// var variable = 10;
+// (()=>{
+//     //memory allocation -> variable_3 -> 45, variable_2 -> 15
+//     //
+//     // variable_3 = 35;
+//     console.log(variable_3)//undefined
+//     var variable_3 = 45;
+//     variable_2 = 15;//
+//     console.log(variable)//->10
+// })()
+// console.log(variable_2)//15
+// console.log(variable_3)//->error
+// var variable = 30;
+
+//c -> undefned -> 10
+// var x = 10;
+// console.log(x)//10
+
+// if(true){
+//     (function(){
+//         var x = 20;
+//         console.log(x)//20
+//     })()
+//     console.log(x)//10
+// }
+// console.log(x)//10
+
+// var x = 10;//defined
+// var x = 20;//defined -> no error
+// console.log(x)//20
+
+// let y = 10;//error
+// function sample(){
+//     let y = 20;
+// }
+// let y;
+// const perm = 110;
+
+// perm = 20;//not allowed -> error
+
+
+//global scope -> x
+//let and const -> 
+
+// console.log(x) //undefined
+// const z = 300;
+// var x = 20;
+// // y=100;
+// // let y = 200;
+// console.log(y) //temporal dead zone
+// let y; //undfined 
+// console.log(z)
+// console.log(y)//undefined
+// y= 200; //
+
+//var -> functioned scope
+//let and const -> block scoped 
+/* 
+    if{},
+    for(){},
+    function(){},
+    {}
+*/
+// let blockScopeChecck = 0;//parent scope
+// {
+//     let blockScopeChecck = 20;
+//     console.log(blockScopeChecck) 
+// }
+// if(true) 
+// {
+//     let blockScopeChecck = 200;
+//     var notaBlockScopeButAFunctionScope = 'something'//global 
+//     console.log(blockScopeChecck)
+// }
+// (
+//     function(){
+//         var notBlockScope = 1000000;
+//         let blockScopeChecck = 2000;
+//         console.log(blockScopeChecck)
+//     }
+// )()
+// console.log(blockScopeChecck) //error
+
+// (function functionedScope(){
+//     var functionS= 'some';
+// })
+// console.log(functionS) //error
+
+
+// let sampleLet = 10;
+// // if(true){
+// //      let sampleLet = 20;
+// // }
+// function sample(){
+//     var sampleLet = 200;
+//     {
+//         let sampleLet = 300;
+//     }
+//     const sampleLet = 400;/* let and const work exactly same for scoping */
+// }
+// sample()
+
+// var test = 20;
+// let test = 10;
+
+
+var sampleTest = 200;//let to var
+function test(){
+    console.log(sampleTest)//600 //previous concept //undefiened
+    var sampleTest = 600;// 
+    {
+        console.log(sampleTest)//error //temporal dead zone -> undefined //error
+        var sampleTest = 400;//let to var
+    }
+}
+{
+    sampleTest = 500;
+}
+test()
+console.log(sampleTest)//500
+
+//some more 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
