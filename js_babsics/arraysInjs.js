52ced1d1cf84b9585fdd08a19384e536da9586d9
Naman Jindal <namanjindal113@gmail.com>
2022-10-17 23:01:54 +0530

    arrays further concepts

--- js_babsics/arraysInjs.js
+++ js_babsics/arraysInjs.js
@@ -120,61 +120,61 @@
 //     return arr;
 // }
 
-var arr = [2,4,6,11,12,14,15]
+// var arr = [2,4,6,11,12,14,15]
 
-var arr2 = arr.filter((value)=>{
-    return value;//true, false, treu, true
-})
-console.log(arr);//[2,4,6,11,12,14,15]
-console.log(arr2);//[2,4,6,11,12,14,15]
-arr2.push(100);
-console.log(arr);//[2,4,6,11,12,14,15]
-console.log(arr2);//[2,4,6,11,12,14,15, 100]
+// var arr2 = arr.filter((value)=>{
+//     return value;//true, false, treu, true
+// })
+// console.log(arr);//[2,4,6,11,12,14,15]
+// console.log(arr2);//[2,4,6,11,12,14,15]
+// arr2.push(100);
+// console.log(arr);//[2,4,6,11,12,14,15]
+// console.log(arr2);//[2,4,6,11,12,14,15, 100]
 
 
-//result and arr -> 
+// //result and arr -> 
 
-//shallow 
+// //shallow 
 
-// var abc = 1; //[1,2,3,4]
-// var def = 1;//[1,2,3,4]
+// // var abc = 1; //[1,2,3,4]
+// // var def = 1;//[1,2,3,4]
 
-// var abc = 1;
+// // var abc = 1;
 
-// var abc = [1,2,3,4];//differnet addres -1
-// var def = []; //different address -2
-// for(let i=0; i< abc.length; i++){
-//     def.push(abc[i]);
-// }
-// def.push(10000)
-// console.log(abc);//
-// console.log(abc);//
-// console.log(def);//
+// // var abc = [1,2,3,4];//differnet addres -1
+// // var def = []; //different address -2
+// // for(let i=0; i< abc.length; i++){
+// //     def.push(abc[i]);
+// // }
+// // def.push(10000)
+// // console.log(abc);//
+// // console.log(abc);//
+// // console.log(def);//
 
-//deep copy happeneded above
+// //deep copy happeneded above
 
-//looping of an array -> 
+// //looping of an array -> 
 
-//for of 
-// let profiles = ['akash', 'aksay','ashutosh'];
+// //for of 
+// // let profiles = ['akash', 'aksay','ashutosh'];
 
-// for(const anything of profiles){
-//     console.log(anything);
-// }
+// // for(const anything of profiles){
+// //     console.log(anything);
+// // }
 
-//123456
-//123457
-//123458
+// //123456
+// //123457
+// //123458
 
-const profiles = ['akash', 'aksay','ashutosh'];
-const obj ={}
+// const profiles = ['akash', 'aksay','ashutosh'];
+// const obj ={}
 // console.log(profiles)
 // const profiles = 20;
 // profiles.push('naman') //
 // console.log(profiles)
 
-const abc = 10;
-abc = 100;
+// const abc = 10;
+// abc = 100;
 
 /* 
     arr(10000000) ->
@@ -208,3 +208,138 @@ abc = 100;
     hasmap through a random generator -> random index ->
     regenrate ->
 */
+// (function check(){
+
+//     console.log('cehcck');
+// })();
+
+
+
+// var something2 = (()=>{
+//     console.log('not check')
+// })()
+
+
+// // // check();
+
+// var something = (()=>{
+//     console.log('something')
+// })()
+
+// something();
+// (()=>{
+//     var sampleVar = 10;
+//     console.log('anythign')
+
+//     function abcd(){
+//         console.log('abcd')
+//     }
+//     abcd();
+// })()
+// var sampleVar;
+// console.log(sampleVar) //
+
+// console.log(['delhi', 'tamil nadu', 'karnatka', 'punjab'].reverse()); //
+// console.log(['delhi', 'tamil nadu', 'karnatka', 'punjab'].toString()) //
+
+// let sample = ['delhi', 'tamil nadu', 'karnatka', 'punjab'];
+// let sample2 = ['a', 'b', 'c', 'd']
+// let sample3 = ['e', 'f', 'g']
+// console.log(sample.concat(sample2).concat(sample3)); //deep copy
+// console.log(sample) 
+//  //
+// // let sample
+// console.log(sample.sort().reverse()); //string
+// /* ascii conversion */
+// let sample4 = [100, 20, 500 ,200, 400, 5];
+
+// /* comparator function? */
+// console.log(sample4.sort(function(a,b){return a-b})) //
+// console.log(sample4.sort(function(a,b){return b-a})) //
+  
+
+// console.log(sample.join(" ' ")) //delhi ' tamil nadu ' 
+// // console.log(['delhi', 'tamil nadu', 'karnatka', 'punjab']).from()
+
+
+// let states = ['delhi', 'tamil nadu', 'karnatka', 'punjab'];
+// console.log(states.slice(1)) //range -> 
+// console.log(states)
+
+
+// console.log(states.splice(2, 2)); //
+//var spliceCheck = ; //states.splice(startIndex, quantity(number), things_to_appends)
+//  [
+//     "delhi",
+//     "tamil nadu",
+//     "hello",
+//     "bye",
+//     "punjab"
+// ]
+
+// console.log(spliceCheck)
+// console.log(states)//
+
+//...
+// let states = ['delhi', 'tamil nadu', 'karnatka', 'punjab'];
+// let names = ['a', 'b', 'c', 'd']
+// let blankArr = [];
+// let combined = [...states, ...names];
+// console.log(combined)
+
+// let statesDeepCopy = [...states];
+// statesDeepCopy.push('haryana')
+
+// console.log(statesDeepCopy);
+// console.log(states)
+
+/* default values in functions */
+// function checkRestOpt(name, name2, name3, name4, name5= 'naman'){ //rest operator
+//     // console.log('name', name)
+//     // console.log('name2', name2)
+//     // console.log(aplo[0])
+//     // console.log(aplo[1])
+//     console.log(name5)
+// }
+
+// checkRestOpt('a', 'b', 'c', 'd', 'e')
+// checkRestOpt('a', 'b', 'c', 'd')
+
+// let temp = ['one', 'two']
+// let states = ['delhi', 'tamil nadu', 'karnatka', 'punjab', ['aabcd', 'defg']];
+// let states2 = states; //shallow copy
+// let statesDeep = [...states]; //deep copy
+
+// console.log(states === states2); //true
+// console.log(states === statesDeep); //false
+
+// // let arr2 = 
+// console.log(states)
+// let cp = [...states] 
+// cp.push('appended')
+// console.log(cp)//
+
+// console.log(states)
+
+// console.log(first)
+
+
+//States(1) -> a [1,2,3,4,56]
+//states2(2) -> a 
+//stateDeepCopy(3) -> c [1,2,3,4,56]
+//states4(4) -> d [1,2,3,4,56]
+
+// let states = [1,2,3,4,56];
+// let states2 = [1,2,3,4,56];
+// let states3 = [1,2,3,4,56];
+// states2 = states;
+// let states4 = [...states];
+
+//looping mechanism -> 
+
+//map 
+['abcd','defg','cde','dewfwe'].map((val, index)=>{
+    console.log(val)
+    console.log(index)
+})
+
